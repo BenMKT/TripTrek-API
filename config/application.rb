823cef00b5f3e71fd108d8e8461826b9adcdb3ec
devalibello <alibelloimoukhuede@gmail.com>
2023-12-09 16:27:02 +0300

    Add render config

--- config/application.rb
+++ config/application.rb
@@ -3,12 +3,12 @@
 require_relative 'boot'
 
 require 'rails/all'
-require 'sprockets/railtie'
+# require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
-# Bundler.require(:default, :assets, Rails.env)
+# Bundler.require(*Rails.groups)
+Bundler.require(:default, :assets, Rails.env)
 
 module FinalCapstoneProjectApi
   class Application < Rails::Application
